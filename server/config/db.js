dd126b4fbab6818eec7012ea79f60721b7af60e8
chinmayaramani <chinmayashivalingappa.ramani@ontariotechu.net>
2023-11-16 20:04:15 -0500

    Final update

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 // Exporting a configuration object for MongoDB connection
 module.exports =
 {
-    "URI": "mongodb+srv://Chinmayaramani:Chsh43ra@mongodbserver.ambl0tp.mongodb.net/workOut"
+    "URI": "mongodb+srv://Chinmayaramani:kChtGrCJDybGT4nI@mongodbserver.ambl0tp.mongodb.net/workOut"
 }
